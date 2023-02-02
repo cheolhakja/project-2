864af4cf73b01d096287ca0543fdea70579a2569
cheolhakja <rebegin15@gmail.com>
2023-02-02 14:42:43 +0900

    refactor: 테스트 수정

--- reviewanalyzetest/preprocess/test_load_json.py
+++ reviewanalyzetest/preprocess/test_load_json.py
@@ -1,22 +1,15 @@
 import unittest
+import reviewanalyze.preprocess.load_json
 
 path_prefix = "hello"
 path_suffix = ["James", "Richman"]
 
 
-def return_path(path_prefix, path_list_suffix):
-    path_list = []
-    for i in path_list_suffix:
-        path = path_prefix + i
-        path_list.append(path)
-
-    return len(path_list), path_list
-
-
 class CreateFilePathTest(unittest.TestCase):
-    def test_file_path_creation(self):
+    def test_file_path_create_func(self):
         self.assertEquals(
-            return_path(path_prefix, path_suffix), (2, ["helloJames", "helloRichman"])
+            reviewanalyze.preprocess.load_json.return_path(path_prefix, path_suffix),
+            (["helloJames", "helloRichman"], 2),
         )
 
 
