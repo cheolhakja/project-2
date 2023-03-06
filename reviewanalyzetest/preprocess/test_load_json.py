53475e52796cdd2cdce12c98bab367262cca1ffd
cheolhakja <rebegin15@gmail.com>
2023-03-06 17:35:57 +0900

    test: 검증 데이터를 담고 있는 파일 불러오기 테스트

--- reviewanalyzetest/preprocess/test_load_json.py
+++ reviewanalyzetest/preprocess/test_load_json.py
@@ -1,17 +1,13 @@
 import unittest
-import reviewanalyze.preprocess.load_json
+import reviewanalyze.preprocess.iterate_create_path
 
-path_prefix = "hello"
-path_suffix = ["James", "Richman"]
-
-
-class CreateFilePathTest(unittest.TestCase):
-    def test_file_path_create_func(self):
+class TestLoadFile(unittest.TestCase):
+    def test_load_file_func(self):
+        list_test = reviewanalyze.preprocess.iterate_create_path.return_review_rating_list_using_parameter(
+            "C:\\Users\\IBK\\Desktop\\나만의-우테코\\kakaomap-data-scrap\\reviewdata\\testing\\", R"C:\Users\IBK\Desktop\나만의-우테코\kakaomap-data-scrap\reviewdata\testing")
         self.assertEquals(
-            reviewanalyze.preprocess.load_json.return_path(path_prefix, path_suffix),
-            (["helloJames", "helloRichman"], 2),
+            len(list_test), 113
         )
-
 
 if __name__ == "__main__":
     unittest.main()
